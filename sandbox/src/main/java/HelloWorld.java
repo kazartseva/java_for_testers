676293d5962b55e203233d13a270d904af992ff3
Maria Kazartseva <kazarceva@gmail.com>
2022-02-20 20:38:13 +0100

    Лекция 1.9. Методы: функции, ассоциированные с объектами

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -9,23 +9,14 @@ public class HelloWorld {
 		
 		
 		Square s = new Square(5);
-		System.out.println("площадь квадрата со стороной " + s.l + " равна " + area(s));
+		System.out.println("площадь квадрата со стороной " + s.l + " равна " + s.area());
 		
 		Rectangle r = new Rectangle(4,5);
-		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 	
 	public static void hello(String somebody) {
 		System.out.println("Hello, "+ somebody + "!");
 	}
-	
-	public static double area (Square s) {
-		return s.l * s.l;
-	}
-	
-	public static double area (Rectangle r) {
-		return r.a * r.b;
-	}
-	
 
 }
