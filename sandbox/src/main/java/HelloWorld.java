3a137ea64113117cdff19f1ac5b6625db034a4ae
Maria Kazartseva <kazarceva@gmail.com>
2022-02-20 20:27:31 +0100

    Лекция 1.8. Классы и объекты

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -8,25 +8,24 @@ public class HelloWorld {
 		hello("Masha");
 		
 		
-		double len = 5;
-		System.out.println("площадь квадрата со стороной " + len + " равна " + area(len));
+		Square s = new Square(5);
+		System.out.println("площадь квадрата со стороной " + s.l + " равна " + area(s));
 		
-		
-		double a = 4;
-		double b = 5;
-		System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+		Rectangle r = new Rectangle(4,5);
+		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 	
 	public static void hello(String somebody) {
 		System.out.println("Hello, "+ somebody + "!");
 	}
 	
-	public static double area (double l) {
-		return l * l;
+	public static double area (Square s) {
+		return s.l * s.l;
 	}
 	
-	public static double area (double a, double b) {
-		return a * b;
+	public static double area (Rectangle r) {
+		return r.a * r.b;
 	}
+	
 
 }
---
+++ sandbox/src/main/java/Square.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Square {
+	
+	public double l;
+	
+	public Square(double l) {
+		this.l = l;
+	}
+
+}
