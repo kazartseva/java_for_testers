7914ce5aca6cd3c47902e8d1b33b5631826220be
Maria Kazartseva <kazarceva@gmail.com>
2022-02-20 19:46:51 +0100

    добавлено несколько функций (Лекция 1.7)

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -4,7 +4,29 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello, world!");
+		hello("Misha");
+		hello("Masha");
+		
+		
+		double len = 5;
+		System.out.println("площадь квадрата со стороной " + len + " равна " + area(len));
+		
+		
+		double a = 4;
+		double b = 5;
+		System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+	}
+	
+	public static void hello(String somebody) {
+		System.out.println("Hello, "+ somebody + "!");
+	}
+	
+	public static double area (double l) {
+		return l * l;
+	}
+	
+	public static double area (double a, double b) {
+		return a * b;
 	}
 
 }
